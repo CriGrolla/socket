ff23f9a0c0dea17cc4734a5a766db9dd648e8b32
CriGrolla <crygro02@gmail.com>
2021-02-03 13:59:49 +0100

    L3

--- client.py
+++ client.py
@@ -1,19 +1,50 @@
 #!/usr/bin/env python3
 
-import socket
+input_string = 'Hello'
+print(type(input_string))
+input_bytes_encoded = input_string.encode()
+print(type(input_bytes_encoded))
+print(input_bytes_encoded)
+output_string=input_bytes_encoded.decode()
+print(type(output_string))
+print(output_string)
 
-SERVER_ADDRESS = '127.0.0.1'  # The server's hostname or IP address
-SERVER_PORT = 65432        # The port used by the server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((SERVER_ADDRESS, SERVER_PORT))
-dati = input("Inserisci messaggio per il server: ")
-dati = dati.encode()
-# Send data to server
-s.send(dati)
-# Receive response from server
-dati = s.recv(2048)
-if dati:
-    # Convert back to string for python3
-    dati = dati.decode()
-    print("Ho ricevuto dal server: ")
-    print(dati + '\n')
+import socket #importiamo il pacchetto socket
+
+SERVER_ADDRESS = '127.0.0.1' #indirizzo server
+SERVER_PORT = 22224 #porta server
+
+sock_service = socket.socket() #crea la richiesta del servizio
+
+sock_service.connect((SERVER_ADDRESS, SERVER_PORT)) #invia la richiesta del servizio e crea la richiesta
+
+print("Connesso a " + str((SERVER_ADDRESS, SERVER_PORT))) #comando per verificare che il collegamento sia in funzione
+while True:
+    try:
+        dati = input("Inserisci i dati da inviare (0 per terminare la connessione): ") #l'utente inserisce la richiesta da mandare al server
+    except EOFError: #se trova un errore sulla rete chiude la connessione
+        print("\nOkay. Exit")
+        break
+    if not dati: #controllo che la riga non sia vuota
+        print("Non puoi inviare una stringa vuota!") 
+        continue
+    if dati == '0': 
+        print("Chiudo la connessione con il server!") #quando l'utente unserisce '0' in input si interrompe la connessione
+        break
+    
+    dati = dati.encode() #vengono decodificati i dati
+
+    sock_service.send(dati) #vengono inviati i dati
+
+    dati = sock_service.recv(2048) #aspetta la risposta dal server
+
+    if not dati: #controllo risposta del server
+        print("Server non risponde. Exit")
+        break # se non risponde chiude il collegamento
+    
+    dati = dati.decode() # se risponde vengono decodificati i dati
+
+    print("Ricevuto dal server:")
+    print(dati + '\n') # i dati ricevuti vengono stampati
+
+sock_service.close() #se l'utente inserisce 0 o se il server non risponde, si chiude la connessione
--- server.py
+++ server.py
@@ -1,31 +1,37 @@
 #!/usr/bin/env python3
-#https://realpython.com/python-sockets/
-import socket
+import socket #importiamo il pacchetto socket
 
-HOST = '127.0.0.1'  # Indirizzo dell'interfaccia standard di loopback (localhost)
-PORT = 65432        # Porta di ascolto, la lista di quelle utilizzabili parte da >1023)
+SERVER_ADDRESS = '127.0.0.1' #indirizzo server
+SERVER_PORT = 22224 #porta server
 
-s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock_listen = socket.socket() #
+sock_listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #
+sock_listen.bind((SERVER_ADDRESS, SERVER_PORT)) #
+sock_listen.listen(5) #
 
-# Optionale: permette di riavviare subito il codice,
-# altrimenti bisognerebbe aspettare 2-4 minuti prima di poter riutilizzare(bindare) la stessa porta
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+print("Server in ascolto su %s." % str((SERVER_ADDRESS, SERVER_PORT))) #il server è in ascolto e in grado di ricevere richieste di connessione
 
-s.bind((HOST, PORT))
-s.listen()
-print("[*] In ascolto su %s:%d" % (HOST, PORT))
-clientsocket, address = s.accept()
-with clientsocket as cs:
-    print('Connessione da', address)
-    while True:
-        dati = cs.recv(1024)
-        dati.decode()
-        if not dati:
-            break
-        dati = dati.decode()
-        print("Ricevuto '%s' dal client" % dati)
-        dati = "Ciao, " + str(address) + ". Ho ricevuto questo: '" + dati + "'"
-        dati = dati.encode()
-        # Invia i dati modificati al client
-        cs.send(dati)
-        print('Inviato al client:', dati)
+while True: #se il server è in ascolto esegue i comandi
+    sock_service, addr_client = sock_listen.accept() #acetta la connessione con il client e rimane in ascolto per ricevere i dati
+    print("\nConnessione ricevuta da " + str(addr_client))
+    print("\nAspetto di ricevere i dati ")
+    contConn=0 #inizializza il contatore
+    while True: #se la connessione è attiva esegue i comandi
+        dati = sock_service.recv(2048) #aspetta la richiesta dal client
+        contConn+=1 #aumenta il contatore di 1
+        if not dati: #controlla che dai abbia un valore
+            print("Fine dati dal client. Reset")
+            break #se dati non ha valore chiude la connessione
+        
+        dati = dati.decode() #se dati ha valore lo decodifica
+        print("Ricevuto: '%s'" % dati) 
+        if dati=='0': 
+            print("Chiudo la connessione con " + str(addr_client))
+            break #se dati ha valore '0' chiude la connessione
+        dati = "Risposta a : " + str(addr_client) + ". Il valore del contatore è : " + str(contConn) #se dati ha valore !='0' restituisce al client il valore del contatore
+
+        dati = dati.encode() #codifica la risposta
+
+        sock_service.send(dati) #invia i dati codificati
+
+    sock_service.close() #fine ascolto del server
