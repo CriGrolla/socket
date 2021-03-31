af6f2f9458fb9306f81bd4791293b984d04e4fd4
CriGrolla <crygro02@gmail.com>
2021-03-31 13:02:14 +0200

    L3_handshake

--- client.py
+++ client.py
@@ -1,14 +1,3 @@
-#!/usr/bin/env python3
-
-input_string = 'Hello'
-print(type(input_string))
-input_bytes_encoded = input_string.encode()
-print(type(input_bytes_encoded))
-print(input_bytes_encoded)
-output_string=input_bytes_encoded.decode()
-print(type(output_string))
-print(output_string)
-
 import socket #importiamo il pacchetto socket
 
 SERVER_ADDRESS = '127.0.0.1' #indirizzo server
@@ -19,23 +8,14 @@ sock_service = socket.socket() #crea la richiesta del servizio
 sock_service.connect((SERVER_ADDRESS, SERVER_PORT)) #invia la richiesta del servizio e crea la richiesta
 
 print("Connesso a " + str((SERVER_ADDRESS, SERVER_PORT))) #comando per verificare che il collegamento sia in funzione
+protocollo= ["SYN", "SYN ACK", "ACK with data", "ACK for data"]
+dati = '0'
 while True:
-    try:
-        dati = input("Inserisci i dati da inviare (0 per terminare la connessione): ") #l'utente inserisce la richiesta da mandare al server
-    except EOFError: #se trova un errore sulla rete chiude la connessione
-        print("\nOkay. Exit")
-        break
-    if not dati: #controllo che la riga non sia vuota
-        print("Non puoi inviare una stringa vuota!") 
-        continue
-    if dati == '0': 
-        print("Chiudo la connessione con il server!") #quando l'utente unserisce '0' in input si interrompe la connessione
-        break
-    
-    dati = dati.encode() #vengono decodificati i dati
+    print("Invio: " + dati + " - " + protocollo[int(dati)])
+    dati = dati.encode() #vengono codificati i dati
 
     sock_service.send(dati) #vengono inviati i dati
-
+    
     dati = sock_service.recv(2048) #aspetta la risposta dal server
 
     if not dati: #controllo risposta del server
@@ -43,8 +23,15 @@ while True:
         break # se non risponde chiude il collegamento
     
     dati = dati.decode() # se risponde vengono decodificati i dati
+    if dati=='3':
+        print("Ricevuto: " + dati + " - " + protocollo[int(dati)])
+        print("Termino connessione")
+        break
+    else:
+        print("Ricevuto: " + dati + " - " + protocollo[int(dati)])
+        dati=int(dati)+1
+        dati=str(dati)
 
-    print("Ricevuto dal server:")
-    print(dati + '\n') # i dati ricevuti vengono stampati
 
 sock_service.close() #se l'utente inserisce 0 o se il server non risponde, si chiude la connessione
+#print("Termino connessione")
--- server.py
+++ server.py
@@ -10,26 +10,24 @@ sock_listen.bind((SERVER_ADDRESS, SERVER_PORT)) #
 sock_listen.listen(5) #
 
 print("Server in ascolto su %s." % str((SERVER_ADDRESS, SERVER_PORT))) #il server è in ascolto e in grado di ricevere richieste di connessione
+protocollo= ["SYN", "SYN ACK", "ACK with data", "ACK for data"]
 
 while True: #se il server è in ascolto esegue i comandi
     sock_service, addr_client = sock_listen.accept() #acetta la connessione con il client e rimane in ascolto per ricevere i dati
     print("\nConnessione ricevuta da " + str(addr_client))
     print("\nAspetto di ricevere i dati ")
-    contConn=0 #inizializza il contatore
     while True: #se la connessione è attiva esegue i comandi
         dati = sock_service.recv(2048) #aspetta la richiesta dal client
-        contConn+=1 #aumenta il contatore di 1
         if not dati: #controlla che dai abbia un valore
             print("Fine dati dal client. Reset")
             break #se dati non ha valore chiude la connessione
         
         dati = dati.decode() #se dati ha valore lo decodifica
-        print("Ricevuto: '%s'" % dati) 
-        if dati=='0': 
-            print("Chiudo la connessione con " + str(addr_client))
-            break #se dati ha valore '0' chiude la connessione
-        dati = "Risposta a : " + str(addr_client) + ". Il valore del contatore è : " + str(contConn) #se dati ha valore !='0' restituisce al client il valore del contatore
-
+        print("Ricevuto: " + dati + " - " + protocollo[int(dati)])
+        dati=int(dati)+1
+        dati=str(dati)
+        
+        print("Invio: " + dati + " - " + protocollo[int(dati)])
         dati = dati.encode() #codifica la risposta
 
         sock_service.send(dati) #invia i dati codificati
